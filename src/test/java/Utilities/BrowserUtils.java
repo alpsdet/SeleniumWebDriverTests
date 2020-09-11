c8d1d40f84213f9eb44c50f04617e852bfe849be
Alp Aslan <alpsdet@gmail.com>
2020-09-10 23:09:59 -0400

    Additional Commits

---
+++ src/test/java/Utilities/BrowserUtils.java
@@ -0,0 +1,13 @@
+package Utilities;
+
+public class BrowserUtils {
+
+    public static void wait(int seconds) {
+        try {
+            Thread.sleep((long)(1000 * seconds));
+            }
+        catch (InterruptedException var2) {
+            var2.printStackTrace();
+        }
+    }
+}
